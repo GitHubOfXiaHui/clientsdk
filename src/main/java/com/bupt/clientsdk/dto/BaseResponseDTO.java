7f0ee6e31a09bd94263c2fb98816767b37b8f3a6
xiahui <515065283@qq.com>
2017-08-30 18:57:59 +0800

    debug

--- src/main/java/com/bupt/clientsdk/dto/BaseResponseDTO.java
+++ src/main/java/com/bupt/clientsdk/dto/BaseResponseDTO.java
@@ -22,8 +22,24 @@ public abstract class BaseResponseDTO implements Serializable {
 		return (T) obj;
 	}
 	
+	public boolean isSuccess() {
+		return code != null && code == ResponseEnum.SUCCESS.getCode();
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
 	public String getMsg() {
 		return msg;
 	}
 
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
 }
